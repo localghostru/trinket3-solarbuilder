f609d63031de33d3378b038f41f65c97691a5bc8
Daniel Sokolov <daniil.a.sokolov@gmail.com>
2014-04-06 22:10:00 +0400

    Scaling the camera. Sun is now at world (0, 0)

--- loader.js
+++ loader.js
@@ -1,10 +1,10 @@
-Main = { width: 640, height: 480 };
+Main = { width: 800, height: 600 };
 
 Main.Loader = function(game) {};
 
 Main.Loader.prototype = {
     preload:  function() {
-        game.stage.backgroundColor = '#000018';
+        game.stage.backgroundColor = '#000020';
     },
     
     create: function() {
@@ -39,5 +39,66 @@ Common = {
     getRandomSpeed: function(sun, planet) {
         var first = this.getFirstSpeed(sun, planet);
         return first + Math.random() * (this.getSecondSpeed(sun, planet) - first) / 4;
+    },
+    
+    checkCollision: function(body1, body2) {
+        var dist = this.getSquaredDistance(body1, body2);
+        return (dist < Math.pow(body1.radius + body2.radius, 2));
+    },
+    
+    checkGroupCollision: function(bodies, callback) {
+        var i, j, first, second;
+        for(i = 0; i < bodies.length; i++) {
+            first = bodies.getAt(i);
+            for(j = i + 1; j < bodies.length; j++) {
+                second = bodies.getAt(j);
+                if (this.checkCollision(first, second) && typeof(callback) == 'function') {
+                    callback(first, second);
+                }
+            }
+        }   
+    }
+}
+
+// http://stackoverflow.com/questions/1060008/is-there-a-way-to-detect-if-a-browser-window-is-not-currently-active
+// No idea if I'm using this correctly
+systemPauseCheck = function(pauseCallback, resumeCallback) {
+    var hidden = "hidden";
+
+    // Standards:
+    if (hidden in document)
+        document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+        document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+        document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+        document.addEventListener("msvisibilitychange", onchange);
+    // IE 9 and lower:
+    else if ('onfocusin' in document)
+        document.onfocusin = document.onfocusout = onchange;
+    // All others:
+    else
+        window.onpageshow = window.onpagehide 
+            = window.onfocus = window.onblur = onchange;
+
+    function onchange (evt) {
+        var v = 'visible', h = 'hidden',
+            evtMap = { 
+                focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h 
+            };
+
+        evt = evt || window.event;
+        if (evt.type in evtMap)
+            document.body.className = evtMap[evt.type];
+        else        
+            document.body.className = this[hidden] ? "hidden" : "visible";
+        
+        if(document.body.className == h) {
+            pauseCallback();
+        }
+        else {
+            resumeCallback();
+        }
     }
 }
--- playstate.js
+++ playstate.js
@@ -1,28 +1,66 @@
+/* TODOs
+    1. Scaling. --- seems to be done
+    2. Right(?) pane with
+        2.1 Statistics
+        2.2 ...?
+    3. Score.
+    4. Adding planets by time or by will of player/button
+    5. Images instead of bitmapdata
+    6. Planet info on hover
+    
+
+*/
+
 Main.Playstate = function(game) {};
 
 Main.Playstate.prototype = {
-    create: function() {
-        this.bodies = [];
-        this.sun = new Planet(Main.width / 2, Main.height / 2, '#ffff00', 40, 500000);
-        this.planet = new Planet(this.sun.centerX + 100, this.sun.centerY, '#ff00ff', 16, 0.8);
-        this.planet.setVelocity(0, -Common.getRandomSpeed(this.sun, this.planet));
+    create: function() {    
+        game.world.setBounds(-Main.width * 2, -Main.height * 2, Main.width * 4, Main.height * 4);
+        game.camera.focusOnXY(0, 0);
         
-        this.bodies.push(this.sun);
-        this.bodies.push(this.planet);
+        this.bodies = game.add.group();
+        
+        this.sun = new Planet(0, 0, '#fff0a0', 40, 1000000);
+        var planet = new Planet(this.sun.centerX + 100, this.sun.centerY, '#ff00ff', 15, 10);
+        planet.setVelocity(0, -Common.getRandomSpeed(this.sun, planet));
+        
+        this.bodies.add(this.sun);
+        this.bodies.add(planet);
         this.lastUpdateTime = game.time.now;
         
         this.noUpdate = false;
         this.game.onPause.add(this.pause, this);
         this.game.onResume.add(this.resume, this);
+        systemPauseCheck(this.pause, this.resume);
+        
+        upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+        
+        upKey.onDown.add(this.scaleUp);
+        downKey.onDown.add(this.scaleDown);
         
         game.input.onDown.add(this.onGameClick, this);
     },
     
+    scaleUp: function () {
+        game.camera.scale.x -= 0.1;
+        game.camera.scale.y -= 0.1;
+        game.camera.focusOnXY(0, 0);
+    },
+    
+    scaleDown: function () {
+        game.camera.scale.x += 0.1;
+        game.camera.scale.y += 0.1;
+        game.camera.focusOnXY(0, 0);
+    },
+    
     pause: function() {
+//        console.log("Paused: " + game.time.now);
         this.noUpdate = true;
     },
     
     resume: function() {
+//        console.log("Resumed: " + game.time.now);
         this.noUpdate = false;
         this.lastUpdateTime = game.time.now;
     },
@@ -31,37 +69,50 @@ Main.Playstate.prototype = {
         if(this.noUpdate) return;
         var dt = (game.time.now - this.lastUpdateTime)/1000;
         
-        for(var i = 0; i < this.bodies.length; i++) {
-            this.bodies[i].accelerate(this.bodies, dt);
-            this.bodies[i].move(dt);
-            //console.log(this.bodies[i].centerX, this.bodies[i].centerY);
-        }
+        this.bodies.forEach(function(body) {
+                body.accelerate(this.bodies, dt);
+                body.move(dt);
+                }, this);
         
         this.lastUpdateTime = game.time.now;
+        
+        Common.checkGroupCollision(this.bodies, this.collisionHandler);
+    },
+    
+    render: function () {
+        //game.debug.cameraInfo(game.camera, 500, 32);
     },
     
     onGameClick: function(pointer) {
         var size = Math.random() * 15 + 10;
-        var newPlanet = new Planet(pointer.x, pointer.y, '#0000ff', size, Math.sqrt(size)/5);
+        var worldXClicked = (pointer.x - Main.width / 2) / game.camera.scale.x;
+        var worldYClicked = (pointer.y - Main.height / 2) / game.camera.scale.y;
+        var newPlanet = new Planet(worldXClicked, worldYClicked, '#0000ff', size, Math.pow(10, (size / 5) - 2));
         var angle = Common.getAngle(newPlanet, this.sun);        
         var speed = Common.getRandomSpeed(this.sun, newPlanet);
         newPlanet.setVelocity(- speed * Math.sin(angle), speed * Math.cos(angle));
-        this.bodies.push(newPlanet);
+        
+        this.bodies.add(newPlanet);
+    },
+    
+    collisionHandler: function() {
+        console.log("Crash!");
     }
 }
 
 Planet = function(x, y, color, size, mass) {
     this.mass = mass;
+    this.radius = size / 2;
     this.customVel = new Phaser.Point();
     this.centerX = x;
-    this.centerY = y;
+    this.centerY = y;    
     
     var bm = game.add.bitmapData(size, size);
     bm.ctx.fillStyle = color;
-    bm.ctx.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
+    bm.ctx.arc(this.radius, this.radius, this.radius, 0, Math.PI * 2);
     bm.ctx.fill();
     
-    Phaser.Sprite.call(this, game, x - size / 2, y - size / 2, bm);
+    Phaser.Sprite.call(this, game, x - this.radius, y - this.radius, bm);
     game.add.existing(this);
 }
 
@@ -91,16 +142,16 @@ Planet.prototype.accelerate = function(bodies, timePassed) {
 
 Planet.prototype.getResForce = function(bodies) {
     var resultant = new Phaser.Point();
-    var angle, force;
+    var angle, force, other;
     for(var i = 0; i < bodies.length; i++) {
-        if(bodies[i] === this) continue;
+        other = bodies.getAt(i);
+        if(other === this) continue;
         
         // Every force and angle is calculated twice this way. Probably move out of here into general loop?
-        angle = Common.getAngle(this, bodies[i]);
-        force = Common.getForce(this, bodies[i]);
+        angle = Common.getAngle(this, other);
+        force = Common.getForce(this, other);
         resultant.x += force/this.mass * Math.cos(angle);
         resultant.y += force/this.mass * Math.sin(angle);
     }
-    //console.log(resultant);
     return resultant;
 }
